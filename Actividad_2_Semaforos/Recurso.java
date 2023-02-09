5ddda0415708f48711b74fc10dd387e5db924999
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2023-02-09 01:15:55 +0100

    Add files via upload

---
+++ Actividad_2_Semaforos/Recurso.java
@@ -0,0 +1,37 @@
+package Actividad_2_Semaforos;
+
+import java.util.concurrent.Semaphore;
+
+//Creamos la clase Recurso que almacenará los recursos
+public class Recurso {
+
+	// Creamos la variable recursos
+	private int recursos;
+
+	// Semaforo que controla los recursos
+	private Semaphore semaforo;
+
+	// Constructor de recursos
+	public Recurso(int recursos) {
+
+		this.recursos = recursos;
+
+		semaforo = new Semaphore(recursos);
+	}
+
+	// Método que reserva recursos
+	public void reserva(int r) throws InterruptedException {
+
+		// A través del semaforo reservamos los recursos
+		semaforo.acquire(r);
+
+	}
+
+	// Método que libera recursos
+	public void libera(int l) {
+
+		// A través del semaforo liberamos los recursos
+		semaforo.release(l);
+
+	}
+}
